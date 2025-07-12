e15400fc00d28f2c512ace9cd965804be14280c0
N.Supreeth Reddy <cocthops.9@gmail.com>
2025-07-12 20:31:57 +0530

    preloader

--- slider-main/app.js
+++ slider-main/app.js
@@ -17,3 +17,22 @@ function showSlider() {
     // add new active
     items[itemActive].classList.add('active');
 }
+
+const preloader = document.getElementById("preloader");
+const startTime = performance.now();
+
+window.addEventListener("load", () => {
+  const endTime = performance.now();
+  const loadDuration = endTime - startTime;
+
+  const minimumDisplayTime = 0; // 1 second
+
+  const remainingTime = minimumDisplayTime - loadDuration;
+
+  setTimeout(() => {
+    preloader.classList.add("hidden");
+    document.body.classList.remove("hidden-content");
+    document.body.classList.add("reveal-content");
+  }, remainingTime > 0 ? remainingTime : 0);
+});
+
